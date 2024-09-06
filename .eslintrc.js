8d025483b6dcd741fbaf13357c957d73f3f76035
jongwoo Yoo <dbwhddn10@gmail.com>
2024-09-07 01:05:08 +0900

    convert from php code to javascript code

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    projectService: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
+};
